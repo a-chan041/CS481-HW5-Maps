ed9f96ea30921d1fb6ea0bf884429ee3cafb9404
Amanda <chan041@cougars.csusm.edu>
2020-03-23 10:57:44 -0700

    initializing map (currently in Rome)

---
+++ CS481HW5.Android/obj/Debug/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data Hash: E0741596B4A15AF3 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1924
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 504089
+	.include	"typemap.mj.inc"
---
+++ CS481HW5.Android/obj/Debug/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data Hash: E0741596B4A15AF3 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1924
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 504089
+	.include	"typemap.mj.inc"
